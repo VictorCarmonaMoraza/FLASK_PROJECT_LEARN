78510cd9928e1e472cbabcf44eec471b0c09b7fa
Victor <VictorCarmona9035@hotmail.com>
2023-10-25 15:33:00 +0200

    2-Uso de Logging en Flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,4 +6,8 @@ app = Flask(__name__)
 #http://localhost:5000/
 @app.route("/")
 def inicio():
+    #app.logger.debug('Nivel debug')
+    app.logger.info(f'Entramos al path {request.path}')
+    #app.logger.warn('Nivel warning')
+    #app.logger.error('Nivel error')
     return "<p>Hola Mundo desde Flask Victor.!</p>"
