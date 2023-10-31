d49c47b23b29007a88fa9af33e0ee58b8ef97ec8
Victor <VictorCarmona9035@hotmail.com>
2023-10-31 08:11:02 +0100

    Routing y paso de Parámetros con Flask (528)

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ def inicio():
     #app.logger.warn('Nivel warning')
     #app.logger.error('Nivel error')
     return "<p>Hola Mundo desde Flask Victor.!</p>"
+
+##http://localhost:5000/saludar/Victor
+##En variable de tipo string no hace falta declarar el tipo
+@app.route('/saludar/<nombre>')
+def saludar(nombre):
+    return f'Saludos {nombre.upper()}'
+
+##http://localhost:5000/edad/12
+##En este caso el tipo que recibe es un parametro de tipo entero
+@app.route('/edad/<int:edad>')
+def mostrar_edad(edad):
+    return f'Tu edad es: {edad +12}'
+
+##http://localhost:5000/edad/12
+##En este caso el tipo que recibe es un parametro de tipo entero
+@app.route('/edadString/<edad>')
+def mostrar_edad2(edad):
+    return f'Tu edad es: {edad +str(12)}'
